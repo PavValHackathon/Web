c0c6e968e39236ef519b800cae84fc4f28a6c823
PavelBrest <pasha.brest.work@gmail.com>
2021-05-26 18:48:09 +0300

    unit_tests: add unit test project

---
+++ test/PavValHackathon.Web.Common.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace PavValHackathon.Web.Common.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
